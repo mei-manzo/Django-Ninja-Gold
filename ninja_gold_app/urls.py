0ef30fe29d2f480c9714997a2c015937be97cb91
Mei Manzo <mmanzo806@gmail.com>
2021-05-23 23:12:49 -0700

    added pseudo counter to test gold count

--- ninja_gold_app/urls.py
+++ ninja_gold_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    # path('counting_test', views.counting_test),
 ]
--- ninja_gold_app/views.py
+++ ninja_gold_app/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render, redirect, HttpResponse
 
+# def index(request):
+#     return render(request, "index.html")
+
 def index(request):
-    return render(request, "index.html")
+    if 'counter' not in request.session:
+        request.session['counter'] = 0
+    request.session['counter'] += 1
+    return render(request, 'index.html')
